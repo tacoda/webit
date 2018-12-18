d426db2f48a1244cfe1a0f70c8ec746b793aa2d5
Ian Johnson <tacoda@pm.me>
2018-12-17 23:33:42 -0500

    Add click package and add a simple command

--- webit/webit.py
+++ webit/webit.py
@@ -1,4 +1,14 @@
 import boto3
+import click
 
 session = boto3.Session()
 s3 = session.resource('s3')
+
+@click.command('list-buckets')
+def list_buckets():
+    "List all S3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+if __name__ == '__main__':
+    list_buckets()
