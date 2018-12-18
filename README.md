8bc4e26fe07624cdebd6d878d0d952dd3bdbb678
Ian Johnson <tacoda@pm.me>
2018-12-18 00:03:44 -0500

    Restructure into cli group, add list bucket objects command and update README

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 WebIt is a tool that will sync a local directory to an s3 bucket
 and optionally configure Route 23 and CloudFront as well.
+
+## Features
+
+WebIt currently has the following features:
+
+- List buckets
+- List contents of a bucket
--- webit/webit.py
+++ webit/webit.py
@@ -4,11 +4,22 @@ import click
 session = boto3.Session()
 s3 = session.resource('s3')
 
-@click.command('list-buckets')
+@click.group()
+def cli():
+    "WebIt deploys websites to AWS"
+
+@cli.command('list-buckets')
 def list_buckets():
     "List all S3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
 
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List objects in an S3 bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
 if __name__ == '__main__':
-    list_buckets()
+    cli()
