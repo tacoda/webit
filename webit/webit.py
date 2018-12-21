69cc2b07f584bfcddc6b5eb225ce4e355308e49e
Ian Johnson <tacoda@pm.me>
2018-12-20 21:25:49 -0500

    Update code to adhere to PEP8 standards

--- webit/webit.py
+++ webit/webit.py
@@ -4,22 +4,26 @@ import click
 session = boto3.Session()
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "WebIt deploys websites to AWS"
+    """WebIt deploys websites to AWS"""
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all S3 buckets"
+    """List all S3 buckets"""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List objects in an S3 bucket"
+    """List objects in an S3 bucket"""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
 
 if __name__ == '__main__':
     cli()
