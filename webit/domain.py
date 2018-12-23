1aae2d0236b601449ada052c56d6c156841c9093
Ian Johnson <tacoda@pm.me>
2018-12-23 18:37:22 -0500

    Add CloudFront CDN and SSL certificate features

--- webit/domain.py
+++ webit/domain.py
@@ -50,3 +50,24 @@ class DomainManager:
                 }]
             }
         )
+
+    def create_cf_domain_record(self, zone, domain_name, cf_domain):
+        """Create a domain record in zone for the domain name"""
+        return self.client.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'Created by WebIt',
+                'Changes': [{
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': domain_name,
+                        'Type': 'A',
+                        'AliasTarget': {
+                            'HostedZoneId': 'Z2FDTNDATAQYW2',
+                            'DNSName': cf_domain,
+                            'EvaluateTargetHealth': False
+                        }
+                    }
+                }]
+            }
+        )
--- webit/webit.py
+++ webit/webit.py
@@ -17,11 +17,15 @@ import click
 
 from bucket import BucketManager
 from domain import DomainManager
+from certificate import CertificateManager
+from cdn import DistributionManager
 import util
 
 session = None
 bucket_manager = None
 domain_manager = None
+cert_manager = None
+dist_manager = None
 
 
 @click.group()
@@ -29,13 +33,15 @@ domain_manager = None
               help="Use a given AWS profile")
 def cli(profile):
     """WebIt deploys websites to AWS"""
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
     domain_manager = DomainManager(session)
+    cert_manager = CertificateManager(session)
+    dist_manager = DistributionManager(session)
 
 
 @cli.command('list-buckets')
@@ -82,6 +88,34 @@ def setup_domain(domain):
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
     domain_manager.create_s3_domain_record(zone, domain, endpoint)
     print("Domain configured: http://{}".format(domain))
+
+
+@cli.command('find-cert')
+@click.argument('domain')
+def find_cert(domain):
+    """Find a certificate from DOMAIN"""
+    print(cert_manager.find_matching_cert(domain))
+
+
+@cli.command('setup-cdn')
+@click.argument('domain')
+@click.argument('bucket')
+def setup_cdn(domain, bucket):
+    """Set up CloudFront CDN for DOMAIN pointing to BUCKET"""
+    dist = dist_manager.find_matching_dist(domain)
+    if not dist:
+        cert = cert_manager.find_matching_cert(domain)
+        if not cert:
+            print('Error: No matching cert found')
+            return
+        dist = dist_manager.create_dist(domain, cert)
+        print('Waiting for distribution deployment...')
+        dist_manager.await_deploy(dist)
+    zone = domain_manager.find_hosted_zone(domain) \
+           or domain_manager.create_hosted_zone(domain)
+    domain_manager.create_cf_domain_record(zone, domain, dist['DomainName'])
+    print("Domain configured: https://{}".format(domain))
+    return
 
 
 if __name__ == '__main__':
